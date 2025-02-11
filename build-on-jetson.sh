17b69048d14fe852647401d13487dce9ad36e7bd
sebi <alariq@gmail.com>
2025-02-11 22:52:48 +0200

    .gitignore + minor improvement ni build-on-jetson.sh

--- build-on-jetson.sh
+++ build-on-jetson.sh
@@ -3,7 +3,7 @@
 rm -rf ./build
 mkdir build
 cd ./build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_DEPS=NO -DCMAKE_INSTALL_PREFIX=/home/a/external ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_DEPS=NO -DCMAKE_INSTALL_PREFIX=${HOME}/external-test ..
 make -j4
 cd ..
 
